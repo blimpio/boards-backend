59668777b0dc681a0755f1597268e9f12b4197b6
José Padilla <jpadilla@webapplicate.com>
2014-01-20 15:26:26 -0400

    Add comments to utils fields and slugify tests

--- blimp/utils/tests/fields_tests.py
+++ blimp/utils/tests/fields_tests.py
@@ -5,17 +5,31 @@ from ..fields import CharacterSeparatedField
 
 class FieldsTestCase(TestCase):
     def test_field_default_separator_is_comma(self):
+        """
+        Tests that field should have a default comma separator specified.
+        """
         field = CharacterSeparatedField()
         self.assertEquals(field.separator, ',')
 
     def test_field_should_accept_custom_separator(self):
+        """
+        Tests that field should accept a custom separator.
+        """
         field = CharacterSeparatedField(separator='.')
         self.assertEquals(field.separator, '.')
 
     def test_field_to_native_should_return_str_for_given_list(self):
+        """
+        Tests that field's to_native method should return a string
+        from a specified list.
+        """
         field = CharacterSeparatedField()
         self.assertEquals(field.to_native(['a', 'b', 'c']), 'a,b,c')
 
     def test_field_from_native_should_return_list_for_given_str(self):
+        """
+        Tests that field's from_native method should return a list
+        from a specified string.
+        """
         field = CharacterSeparatedField()
         self.assertEquals(field.from_native('a,b,c'), ['a', 'b', 'c'])
