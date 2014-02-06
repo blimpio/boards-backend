23c0fe58d985211f73c9cb8136f18c41774a7cce
José Padilla <jpadilla@webapplicate.com>
2014-02-06 11:17:25 -0400

    Use custom admin for Board and BoardCollaborator

--- blimp/boards/admin.py
+++ blimp/boards/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 from .models import Board, BoardCollaborator
 
 
-admin.site.register([Board, BoardCollaborator])
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name', 'account', 'created_by', 'is_shared', )
+
+
+class BoardCollaboratorAdmin(admin.ModelAdmin):
+    list_display = ('board', 'user', 'invited_user', 'permission', )
+
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(BoardCollaborator, BoardCollaboratorAdmin)
