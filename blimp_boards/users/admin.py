3299224108be7477654dc2ec77beff4633abd72d
José Padilla <jpadilla@webapplicate.com>
2014-02-18 15:22:13 -0400

    Implement custom admin creation for for User model

--- blimp_boards/users/admin.py
+++ blimp_boards/users/admin.py
@@ -1,6 +1,7 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .utils import get_profile_fields
 from .models import User
@@ -11,8 +12,26 @@ class CustomUserChangeForm(UserChangeForm):
         model = User
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(
+            self.error_messages['duplicate_username'],
+            code='duplicate_username',
+        )
+
+
 class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
 
     readonly_fields = ('token_version',)
 
