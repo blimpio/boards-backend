b72b52959e9985ffdb229576460d04ffdb450e97
José Padilla <jpadilla@webapplicate.com>
2014-02-19 15:54:17 -0400

    Add custom admin for Comments app

--- blimp_boards/comments/admin.py
+++ blimp_boards/comments/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
+from ..utils.admin import BaseModelAdmin
 from .models import Comment
 
 
-admin.site.register(Comment)
+class CommentAdmin(BaseModelAdmin):
+    list_display = ('__str__', 'created_by', 'content_type', 'content_object')
+
+admin.site.register(Comment, CommentAdmin)
