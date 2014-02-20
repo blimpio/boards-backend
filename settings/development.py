43b96b145a5c19aeb8ebe93fa077f47b672c3a9e
José Padilla <jpadilla@webapplicate.com>
2014-02-20 14:27:03 -0400

    Setup postgresql as development database

--- settings/development.py
+++ settings/development.py
@@ -29,8 +29,12 @@ DEBUG_TOOLBAR_PATCH_SETTINGS = env_var('DEBUG_TOOLBAR_PATCH_SETTINGS', True)
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'boards',
+        'USER': os.getenv('USER'),
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
