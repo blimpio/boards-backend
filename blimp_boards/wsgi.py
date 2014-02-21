0b162c129331c70347de73a5ff33eaf351f09c14
José Padilla <jpadilla@webapplicate.com>
2014-02-21 18:04:18 -0400

    Use dj-static to serve static files in staging

--- blimp_boards/wsgi.py
+++ blimp_boards/wsgi.py
@@ -24,6 +24,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "settings.%s" % settings)
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
 application = newrelic.agent.WSGIApplicationWrapper(application)
