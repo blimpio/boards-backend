9a76ac7b6e945bae3f8f69365ad3ccd878d84cc7
José Padilla <jpadilla@webapplicate.com>
2014-03-25 19:18:22 -0400

    Increase mime_type max_size

---
+++ blimp_boards/cards/migrations/0008_auto__chg_field_card_mime_type.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Card.mime_type'
+        db.alter_column('cards_card', 'mime_type', self.gf('django.db.models.fields.CharField')(max_length=255))
+
+    def backwards(self, orm):
+
+        # Changing field 'Card.mime_type'
+        db.alter_column('cards_card', 'mime_type', self.gf('django.db.models.fields.CharField')(max_length=40))
+
+    models = {
+        'accounts.account': {
+            'Meta': {'object_name': 'Account'},
+            'allow_signup': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'disqus_shortname': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '255'}),
+            'email_domains': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'null': 'True', 'to': "orm['accounts.EmailDomain']", 'symmetrical': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique': 'True', 'populate_from': "'name'", 'max_length': '50', 'unique_with': '()'})
+        },
+        'accounts.emaildomain': {
+            'Meta': {'object_name': 'EmailDomain', 'ordering': "('-date_modified', '-date_created')"},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'domain_name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'boards.board': {
+            'Meta': {'object_name': 'Board'},
+            'account': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['accounts.Account']"}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'disqus_shortname': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_shared': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'unique_with': "('account',)", 'populate_from': "'name'", 'max_length': '50'}),
+            'thumbnail_lg_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'thumbnail_md_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'thumbnail_sm_path': ('django.db.models.fields.TextField', [], {'blank': 'True'})
+        },
+        'cards.card': {
+            'Meta': {'object_name': 'Card', 'ordering': "['position']"},
+            'board': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['boards.Board']"}),
+            'cards': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'to': "orm['cards.Card']", 'null': 'True', 'symmetrical': 'False', 'related_name': "'+'"}),
+            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.User']"}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'featured': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'file_size': ('django.db.models.fields.IntegerField', [], {'blank': 'True', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_shared': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'mime_type': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '255'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'origin_url': ('django.db.models.fields.URLField', [], {'blank': 'True', 'max_length': '200'}),
+            'position': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            'slug': ('autoslug.fields.AutoSlugField', [], {'blank': 'True', 'unique_with': "('board',)", 'populate_from': "'name'", 'max_length': '50'}),
+            'stack': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'null': 'True', 'to': "orm['cards.Card']", 'related_name': "'+'"}),
+            'thumbnail_lg_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'thumbnail_md_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'thumbnail_sm_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '5'})
+        },
+        'users.user': {
+            'Meta': {'object_name': 'User'},
+            'avatar_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '254'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'gravatar_url': ('django.db.models.fields.URLField', [], {'blank': 'True', 'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'job_title': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '255'}),
+            'last_ip': ('django.db.models.fields.IPAddressField', [], {'blank': 'True', 'null': 'True', 'max_length': '15', 'default': "'127.0.0.1'"}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'timezone': ('django.db.models.fields.CharField', [], {'max_length': '255', 'default': "'UTC'"}),
+            'token_version': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '36', 'default': "'b4dafa98-2307-4e25-a7a3-605924dc24db'"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        }
+    }
+
+    complete_apps = ['cards']
