5c8c4a47e3010979043efea482080874afcac12f
José Padilla <jpadilla@webapplicate.com>
2014-06-03 16:34:03 -0400

    Guess output format for requested previews

--- blimp_boards/files/previews.py
+++ blimp_boards/files/previews.py
@@ -1,11 +1,57 @@
+import os
+import mimetypes
 import jwt
 import requests
 
 from django.conf import settings
 from django.utils.log import getLogger
+from django.utils.six.moves.urllib.parse import urlparse
 
 
 logger = getLogger(__name__)
+
+
+def guess_output_format(url):
+    output_format = 'png'
+    url_path = urlparse(url).path
+    file_extension = os.path.splitext(url_path)[1].lower()
+    file_mimetype, file_encoding = mimetypes.guess_type(url_path)
+
+    if not file_mimetype:
+        return output_format
+
+    file_type = file_mimetype.split('/')[0]
+
+    raw_image_extensions = [
+        '.arw', '.crw', '.cr2', '.nef'
+        '.3fr',
+        '.ari', '.arw',
+        '.bay',
+        '.crw', '.cr2', '.cap',
+        '.dcs', '.dcr', '.dng', '.drf',
+        '.eip', '.erf',
+        '.fff',
+        '.iiq',
+        '.k25', '.kdc',
+        '.mdc', '.mef', '.mos', '.mrw',
+        '.nef', '.nrw',
+        '.obm', '.orf',
+        '.pef', '.ptx', '.pxn',
+        '.r3d', '.raf', '.raw', '.rwl', '.rw2', '.rwz',
+        '.sr2', '.srf', '.srw',
+        '.x3f'
+    ]
+
+    jpg_conditions = [
+        file_type == 'video',
+        file_extension in ['.jpg', '.jpeg', '.tiff', '.tif'],
+        file_extension in raw_image_extensions
+    ]
+
+    if any(jpg_conditions):
+        output_format = 'jpg'
+
+    return output_format
 
 
 def queue_previews(url, sizes, metadata):
@@ -14,7 +60,8 @@ def queue_previews(url, sizes, metadata):
         'url': url,
         'sizes': sizes,
         'metadata': metadata,
-        'extra_data': ['all']
+        'extra_data': ['all'],
+        'format': guess_output_format(url)
     }
 
     token = jwt.encode(payload, settings.BLIMP_PREVIEWS_SECRET_KEY)
