6be680efd28ecc60cb9c4fce6ad90de758156c92
José Padilla <jpadilla@webapplicate.com>
2014-06-09 17:19:08 -0400

    Reset notifications migrations

--- blimp_boards/notifications/migrations/0001_initial.py
+++ blimp_boards/notifications/migrations/0001_initial.py
@@ -8,60 +8,46 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Adding model 'NotificationType'
-        db.create_table('notifications_notificationtype', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('date_created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
-            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
-            ('label', self.gf('django.db.models.fields.CharField')(max_length=40)),
-            ('display', self.gf('django.db.models.fields.CharField')(max_length=50)),
-            ('description', self.gf('django.db.models.fields.CharField')(max_length=100)),
-        ))
-        db.send_create_signal('notifications', ['NotificationType'])
-
         # Adding model 'NotificationSetting'
         db.create_table('notifications_notificationsetting', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('date_created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
-            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('date_created', self.gf('django.db.models.fields.DateTimeField')(blank=True, default=datetime.datetime.now)),
+            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(blank=True, default=datetime.datetime.now)),
             ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['users.User'])),
-            ('notification_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['notifications.NotificationType'])),
+            ('notification_type', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('medium', self.gf('django.db.models.fields.CharField')(max_length=50)),
             ('send', self.gf('django.db.models.fields.BooleanField')()),
         ))
         db.send_create_signal('notifications', ['NotificationSetting'])
 
         # Adding unique constraint on 'NotificationSetting', fields ['user', 'notification_type', 'medium']
-        db.create_unique('notifications_notificationsetting', ['user_id', 'notification_type_id', 'medium'])
+        db.create_unique('notifications_notificationsetting', ['user_id', 'notification_type', 'medium'])
 
         # Adding model 'Notification'
         db.create_table('notifications_notification', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('date_created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
-            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now, blank=True)),
+            ('date_created', self.gf('django.db.models.fields.DateTimeField')(blank=True, default=datetime.datetime.now)),
+            ('date_modified', self.gf('django.db.models.fields.DateTimeField')(blank=True, default=datetime.datetime.now)),
             ('level', self.gf('django.db.models.fields.CharField')(default='info', max_length=20)),
-            ('recipient', self.gf('django.db.models.fields.related.ForeignKey')(related_name='notifications', to=orm['users.User'])),
+            ('recipient', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['users.User'], related_name='notifications')),
             ('unread', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('actor_content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name='notify_actor', to=orm['contenttypes.ContentType'])),
+            ('actor_content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], related_name='notify_actor')),
             ('actor_object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
             ('verb', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-            ('target_content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name='notify_target', to=orm['contenttypes.ContentType'], null=True, blank=True)),
-            ('target_object_id', self.gf('django.db.models.fields.CharField')(null=True, max_length=255, blank=True)),
-            ('action_object_content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name='notify_action_object', to=orm['contenttypes.ContentType'], null=True, blank=True)),
-            ('action_object_object_id', self.gf('django.db.models.fields.PositiveIntegerField')(null=True, blank=True)),
+            ('description', self.gf('django.db.models.fields.TextField')(blank=True, null=True)),
+            ('target_content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], blank=True, related_name='notify_target', null=True)),
+            ('target_object_id', self.gf('django.db.models.fields.PositiveIntegerField')(blank=True, null=True)),
+            ('action_object_content_type', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['contenttypes.ContentType'], blank=True, related_name='notify_action_object', null=True)),
+            ('action_object_object_id', self.gf('django.db.models.fields.PositiveIntegerField')(blank=True, null=True)),
             ('public', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('data', self.gf('jsonfield.fields.JSONField')(null=True, blank=True)),
+            ('data', self.gf('jsonfield.fields.JSONField')(blank=True, null=True)),
         ))
         db.send_create_signal('notifications', ['Notification'])
 
 
     def backwards(self, orm):
         # Removing unique constraint on 'NotificationSetting', fields ['user', 'notification_type', 'medium']
-        db.delete_unique('notifications_notificationsetting', ['user_id', 'notification_type_id', 'medium'])
-
-        # Deleting model 'NotificationType'
-        db.delete_table('notifications_notificationtype')
+        db.delete_unique('notifications_notificationsetting', ['user_id', 'notification_type', 'medium'])
 
         # Deleting model 'NotificationSetting'
         db.delete_table('notifications_notificationsetting')
@@ -72,69 +58,61 @@ class Migration(SchemaMigration):
 
     models = {
         'contenttypes.contenttype': {
-            'Meta': {'unique_together': "(('app_label', 'model'),)", 'ordering': "('name',)", 'db_table': "'django_content_type'", 'object_name': 'ContentType'},
+            'Meta': {'ordering': "('name',)", 'object_name': 'ContentType', 'unique_together': "(('app_label', 'model'),)", 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
         'notifications.notification': {
-            'Meta': {'ordering': "('-date_modified', '-date_created')", 'object_name': 'Notification'},
-            'action_object_content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'notify_action_object'", 'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'action_object_object_id': ('django.db.models.fields.PositiveIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'actor_content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'notify_actor'", 'to': "orm['contenttypes.ContentType']"}),
+            'Meta': {'ordering': "('-date_created',)", 'object_name': 'Notification'},
+            'action_object_content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'blank': 'True', 'related_name': "'notify_action_object'", 'null': 'True'}),
+            'action_object_object_id': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True', 'null': 'True'}),
+            'actor_content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'related_name': "'notify_actor'"}),
             'actor_object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
-            'data': ('jsonfield.fields.JSONField', [], {'null': 'True', 'blank': 'True'}),
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'date_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'data': ('jsonfield.fields.JSONField', [], {'blank': 'True', 'null': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True', 'null': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'level': ('django.db.models.fields.CharField', [], {'default': "'info'", 'max_length': '20'}),
             'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
-            'recipient': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'notifications'", 'to': "orm['users.User']"}),
-            'target_content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'notify_target'", 'to': "orm['contenttypes.ContentType']", 'null': 'True', 'blank': 'True'}),
-            'target_object_id': ('django.db.models.fields.CharField', [], {'null': 'True', 'max_length': '255', 'blank': 'True'}),
+            'recipient': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.User']", 'related_name': "'notifications'"}),
+            'target_content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'blank': 'True', 'related_name': "'notify_target'", 'null': 'True'}),
+            'target_object_id': ('django.db.models.fields.PositiveIntegerField', [], {'blank': 'True', 'null': 'True'}),
             'unread': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'verb': ('django.db.models.fields.CharField', [], {'max_length': '255'})
         },
         'notifications.notificationsetting': {
-            'Meta': {'unique_together': "(('user', 'notification_type', 'medium'),)", 'object_name': 'NotificationSetting'},
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'date_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'Meta': {'object_name': 'NotificationSetting', 'unique_together': "(('user', 'notification_type', 'medium'),)"},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'medium': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'notification_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['notifications.NotificationType']"}),
+            'notification_type': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
             'send': ('django.db.models.fields.BooleanField', [], {}),
             'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['users.User']"})
-        },
-        'notifications.notificationtype': {
-            'Meta': {'object_name': 'NotificationType'},
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'date_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'display': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'label': ('django.db.models.fields.CharField', [], {'max_length': '40'})
         },
         'users.user': {
             'Meta': {'object_name': 'User'},
             'avatar_path': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'date_created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
-            'date_modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'blank': 'True', 'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '254', 'unique': 'True'}),
-            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
-            'gravatar_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'email_notifications': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
+            'gravatar_url': ('django.db.models.fields.URLField', [], {'blank': 'True', 'max_length': '200'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'job_title': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'last_ip': ('django.db.models.fields.IPAddressField', [], {'default': "'127.0.0.1'", 'null': 'True', 'max_length': '15', 'blank': 'True'}),
+            'job_title': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '255'}),
+            'last_ip': ('django.db.models.fields.IPAddressField', [], {'blank': 'True', 'default': "'127.0.0.1'", 'max_length': '15', 'null': 'True'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
-            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'blank': 'True', 'max_length': '30'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'timezone': ('django.db.models.fields.CharField', [], {'default': "'UTC'", 'max_length': '255'}),
-            'token_version': ('django.db.models.fields.CharField', [], {'default': "'d6b25fde-8562-42cc-8d58-c1ac2888270c'", 'max_length': '36', 'unique': 'True', 'db_index': 'True'}),
+            'token_version': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'default': "'de5d72e7-106a-4b74-a912-17fdb1092793'", 'max_length': '36', 'unique': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
         }
     }
